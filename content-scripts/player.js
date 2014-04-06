2a1c2dcc97d0555e27b22bf123415f461daa312b
lramsey <lramsey177@gmail.com>
2014-04-06 13:12:13 -0700

    refactor to add more methods and remove scaling logic from functions. also added z axis to mouse so it will remain on top on pages.

--- content-scripts/player.js
+++ content-scripts/player.js
@@ -11,43 +11,53 @@ window.Player = Player;
   Player.prototype.move = function (endX, endY, duration){
     d3.select('.mouse')
      .transition()
-     .duration(duration)
+     .duration(duration+1000)
      .style({'top':  endY + 'px', 'left': endX + 'px'});
   };
 
   // chains mouse moves together
-  Player.prototype.processData = function(arr, index, xScale, yScale){
-    index = index || 1;
-    xScale = xScale || 1;
-    yScale = yScale || 1;
+  Player.prototype.playRecording = function(arr, index, xScale, yScale){
     if ( index === arr.length ) {
       $('.mouse').detach();
       console.log('movement finished');
     } else {
-      arr[index].t = arr[index].t || 100;
-      var xAdjusted = (arr[index].clientX * xScale);
-      var yAdjusted = (arr[index].clientY * yScale);
-      var xClientOrigin = (arr[index].pageX * xScale) - xAdjusted;
-      var yClientOrigin = (arr[index].pageY * yScale) - yAdjusted;
       //$(window).scrollLeft(xClientOrigin)  $(window).scrollTop(yClientOrigin);
-      this.move(xAdjusted, yAdjusted ,arr[index].t);
+      this.move(arr[index].clientX, arr[index].clientY ,arr[index].t);
       var that = this;
       setTimeout(function(){
-        that.processData(arr, index+1);
-      }, arr[index].t );
+        that.playRecording(arr, index+1);
+      }, arr[index].t +1000);
     }
   };
 
-  Player.prototype.playRecording = function(data){
-    var xScale = $(window).width() / data["width"];
-    var yScale = $(window).height() / data["height"];
-    var movement = data["ticks"];
+  Player.prototype.placeMouse = function(movement){
+    $('body').append('<div class="mouse" style="position:absolute; background: blue; width: 15px; z-index: 9999; height:15px; border-radius: 7.5px; top: '+movement[0].pageY+'px; left:'+movement[0].pageX+'px;"></div>');
+    this.playRecording(movement, 1);
+  };
+
+  Player.prototype.setMoveIntervals = function(movement){
     movement[0].t = 0;
-    $('body').append('<div class="mouse" style="position:absolute; background: red; width: 15px; height:15px; border-radius: 7.5px; top: '+movement[0].pageY+'px; left:'+movement[0].pageX+'px;"></div>');
     for (var i = 1; i < movement.length-1; i++){
       movement[i].t = movement[i]["timestamp"] - movement[i-1]["timestamp"];
     }
-    this.processData(movement, 1, xScale, yScale);
+    movement[movement.length-1].t = 100;
+    this.placeMouse(movement);
+  };
+
+  Player.prototype.scaleXY = function(data){
+    var xScale = $(window).width() / data["width"] || 1;
+    var yScale = $(window).height() / data["height"] || 1;
+    console.log("my width: "+$(window).width() + ", former width: " + data["width"]);
+    console.log("x scale:"+xScale);
+    for(var i = 0; i < data.ticks.length; i++){
+      data.ticks[i].clientX = data.ticks[i].clientX*xScale;
+      data.ticks[i].clientY = data.ticks[i].clientY*yScale;
+      data.ticks[i].pageX = data.ticks[i].pageX*xScale;
+      data.ticks[i].pageY = data.ticks[i].pageY*yScale;
+      data.ticks[i].xClientOrigin = data.ticks[i].pageX-data.ticks[i].clientX;
+      data.ticks[i].yClientOrigin = data.ticks[i].pageY-data.ticks[i].clientY;
+    }
+    this.setMoveIntervals(data.ticks);
   };
 
   Player.prototype.getData = function(clickId){
@@ -57,10 +67,11 @@ window.Player = Player;
       type: 'GET',
       contentType: 'application/json',
       success: function(data){
+        console.log(data);
         if(Array.isArray(data)){
-          data = data[data.length-3];
+          data = data[data.length-1];
         }
-        that.playRecording(data);
+        that.scaleXY(data);
       }
     });
   };
