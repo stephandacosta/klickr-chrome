2423be4a22c391f6b1b1ad9b274893a5281b375f
Hack Reactor Students <github@hackreactor.com>
2014-04-05 19:24:38 -0700

    debugging, please rewrite

--- content-scripts/player.js
+++ content-scripts/player.js
@@ -15,31 +15,22 @@ window.Player = Player;
      .style({'top':  endY + 'px', 'left': endX + 'px'});
   };
 
-  //calculates what the x = 0 and y = 0 points for the client's window is
-  Player.prototype.windowOrigin = function(x, y, arr){
-    var xWindowOrigin = (arr[index].pageX * xScale) - x;
-    var yWindowOrigin = (arr[index].pageY * yScale) - y;
-    //$(window).scrollLeft(xClientOrigin)  $(window).scrollTop(yClientOrigin);
-  };
-
-  // scales x and y for different window sizes, and passes these values to move and windowOrigin
-  Player.prototype.scaleXY = function(arr){
-    var xScale = $(window).width() / data["width"];
-    var yScale = $(window).height() / data["height"];
-    var x = (arr[index].clientX * xScale);
-    var y = (arr[index].clientY * yScale);
-
-    //this.windowOrigin(x, y);
-    this.move(x, y ,arr[index].t);
-  };
-
   // chains mouse moves together
-  Player.prototype.processData = function(arr, index){
+  Player.prototype.processData = function(arr, index, xScale, yScale){
+    index = index || 1;
+    xScale = xScale || 1;
+    yScale = yScale || 1;
     if ( index === arr.length ) {
       $('.mouse').detach();
       console.log('movement finished');
     } else {
       arr[index].t = arr[index].t || 100;
+      var xAdjusted = (arr[index].clientX * xScale);
+      var yAdjusted = (arr[index].clientY * yScale);
+      var xClientOrigin = (arr[index].pageX * xScale) - xAdjusted;
+      var yClientOrigin = (arr[index].pageY * yScale) - yAdjusted;
+      //$(window).scrollLeft(xClientOrigin)  $(window).scrollTop(yClientOrigin);
+      this.move(xAdjusted, yAdjusted ,arr[index].t);
       var that = this;
       setTimeout(function(){
         that.processData(arr, index+1);
@@ -48,13 +39,15 @@ window.Player = Player;
   };
 
   Player.prototype.playRecording = function(data){
+    var xScale = $(window).width() / data["width"];
+    var yScale = $(window).height() / data["height"];
     var movement = data["ticks"];
     movement[0].t = 0;
     $('body').append('<div class="mouse" style="position:absolute; background: red; width: 15px; height:15px; border-radius: 7.5px; top: '+movement[0].pageY+'px; left:'+movement[0].pageX+'px;"></div>');
     for (var i = 1; i < movement.length-1; i++){
       movement[i].t = movement[i]["timestamp"] - movement[i-1]["timestamp"];
     }
-    this.processData(movement, 1);
+    this.processData(movement, 1, xScale, yScale);
   };
 
   Player.prototype.getData = function(clickId){
