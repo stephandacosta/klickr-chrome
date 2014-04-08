96abdb8b3d3f8f65f3dd172eb49c76daf2f8b3f4
Willson Mock <willson.mock@gmail.com>
2014-04-08 15:26:45 -0700

    Move around different parts within addListener part of background.js - not changing code

--- bg/background.js
+++ bg/background.js
@@ -84,33 +84,33 @@ chrome.tabs.onUpdated.addListener(function(){
 
 // listener on saver box (replay, save, share) and recorder (stage)
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // Replay recording: requests player to play staged recording
-  if (request.action === 'replay') {
-    console.log('Background: Replay recording');
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "playStagedKlick", klick: window.stagedKlick}, function(response) {
-        console.log(response);
-      });
-    });
-    sendResponse({response: "Background: Processed replay message"});
   
+  // Stage recording: updates background with staged recording sent from recorder.js
+  if (request.action === 'stage') {
+    console.log('Background: Stage recording in background');
+    window.stagedKlick = request.klick;
+    sendResponse({response: "background: processed stage message"});
+  }
+
+  // Replay recording: requests player to play staged recording
+  else if (request.action === 'replay') {
+    console.log('Background: Replay recording');
+    helpers.activeTabSendMessage({action: "playStagedKlick", klick: window.stagedKlick});
+    sendResponse({response: "Background: Processed replay message"});
+  }
+
   // Save recording: staged recording is sent to recorder to be pushed to server
-  } else if (request.action === 'save') {
+  else if (request.action === 'save') {
     console.log('Background: Save recording');
     window.stagedKlick.description = request.description;
     window.send(window.stagedKlick); // Background.js should take care of saving the klick object and sending it to the server
     sendResponse({response: "background: processed save message"});
-  
+  }
+
   // Share recording: NEEDS TO BE IMPLEMENTED 
-  } else if (request.action === 'share') {
+  else if (request.action === 'share') {
     console.log('Background: Share recording');
     sendResponse({response: "background: processed share message"});
-  
-  // Stage recording:  updates background with staged recording sent from recorder.js
-  } else if (request.action === 'stage') {
-    console.log('Background: Stage recording in background');
-    window.stagedKlick = request.klick;
-    sendResponse({response: "background: processed stage message"});
   }
 });
 
