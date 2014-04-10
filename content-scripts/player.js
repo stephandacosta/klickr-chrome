d48774a0b52c47a780192d98ec83d25d517a310e
lramsey <lramsey177@gmail.com>
2014-04-09 20:25:33 -0700

    refactor player.js to get ready for multi-page. did not break single page playback.

--- content-scripts/player.js
+++ content-scripts/player.js
@@ -15,44 +15,52 @@ window.Player = Player;
       .style({'top':  endY + 'px', 'left': endX + 'px'});
   };
 
-  Player.prototype.createNewKlick = function(movement, index){
-    movement = movement.slice(index);
+  // this function identical to Recorder.sendToBackground. can factor out later
+  Player.prototype.sendToBackground = function(data){
+    console.log('Recorder: Sending to background');
+    chrome.runtime.sendMessage({action : "stage", klick: data}, function(response){
+      console.log(response);
+    });
+  };
 
+
+  Player.prototype.createNewKlick = function(data, index){
+    data.ticks = data.ticks.slice(index);
+    this.sendToBackground(data);
   };
 
   // chains mouse moves together. also adds the scrolling logic. the pageX and pageY values of the movement object at index are passed to move.
   // function operates recursively, waiting the duration of the prior move in a setTimeout before calling the next move.
-  Player.prototype.playRecording = function(movement, index){
-    //maybe try removing setTimeout
+  Player.prototype.playRecording = function(data, index){
+    var movement = data.ticks;
     if ( index === movement.length ) {
       $('.mouse').detach();
       console.log('movement finished');
     } else {
 
-      // Willson: the idea I have is to look at the current arr[index] and check that object's event.type.
-      // If the event.type is a 'click' event, then redirect to the event.target page first before continuing
-      // // processing playRecording for arr[index + 1].
-   //   if(movement[index].action === 'urlChanged'){
-     //   this.createNewKlick(movement, index);
-      //}
+      if(movement[index].action === 'urlChanged'){
+        this.createNewKlick(data, index);
+      }
       $(window).scrollLeft(movement[index].pageX-movement[index].clientX);
       $(window).scrollTop(movement[index].pageY-movement[index].clientY);
       this.move(movement[index].pageX, movement[index].pageY ,movement[index].t, movement, index);
       var that = this;
       setTimeout(function(){
-        that.playRecording(movement, index+1);
+        that.playRecording(data, index+1);
       }, movement[index].t);
     }
   };
 
   //places the mouse in the dom and gives the mouse's initial position and characteristics
-  Player.prototype.placeMouse = function(movement){
+  Player.prototype.placeMouse = function(data){
+    var movement = data.ticks;
     $('body').append('<div class="mouse" style="position:absolute; background: blue; width: 15px; z-index: 9999; height:15px; border-radius: 7.5px; top: '+movement[0].pageY+'px; left:'+movement[0].pageX+'px;"></div>');
-    this.playRecording(movement, 1);
+    this.playRecording(data, 1);
   };
 
   //uses Date.parse to turn the timestamp value from a date to an integer.  Also establishes the t value of the movement array.
-  Player.prototype.setMoveIntervals = function(movement){
+  Player.prototype.setMoveIntervals = function(data){
+    var movement = data.ticks;
     if(typeof movement[0].timestamp !== 'number'){
       this.parseDate(movement);
     }
@@ -60,7 +68,8 @@ window.Player = Player;
     for (var i = 1; i < movement.length-1; i++){
       movement[i].t = movement[i].timestamp - movement[i-1].timestamp;
     }
-    this.placeMouse(movement);
+    data.ticks = movement;
+    this.placeMouse(data);
   };
 
   //changes the iso date object in timestamp to an integer of the Date.now() format
@@ -83,10 +92,11 @@ window.Player = Player;
       data.ticks[i].pageX = data.ticks[i].pageX*xScale;
       data.ticks[i].pageY = data.ticks[i].pageY*yScale;
     }
-    this.setMoveIntervals(data.ticks);
+    this.setMoveIntervals(data);
   };
 
   //submits an ajax request to the server based on a click id to get movement patterns back
+  // data passed along to every step in this refactor. movement (which is the ticks array) can be derived in the moment.
   Player.prototype.getData = function(clickId){
     var that = this;
     $.ajax({
