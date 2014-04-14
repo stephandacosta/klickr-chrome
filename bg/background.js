d1cd70ecfa7f9ad403c88da361e5233da7a92360
Willson Mock <willson.mock@gmail.com>
2014-04-13 20:13:34 -0700

    Fix bug with recordMessage not showing up on multi-page. Also moved some listeners from bg-player back to background.js

--- bg/background.js
+++ bg/background.js
@@ -86,3 +86,25 @@ window.delete = function () {
   window.rec = undefined;
   window.refreshRecorderStatus(true);
 };
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // Sends server to content scripts
+  if (request.action === 'getServer') {
+    sendResponse({server: Klickr.server});
+  }
+
+  // Save recording: staged recording is sent to recorder to be pushed to server
+  else if (request.action === 'save') {
+    console.log('Background: Save recording');
+    window.rec.addDescription(request.description);
+    window.rec.send();
+    window.rec = undefined;
+    bgPlayer.klickQueue = [];
+    sendResponse({response: "Background: Processed save message"});
+  }
+
+  // If DOM is ready and window.recorderStatus = 'recording', then send message to message.js
+  else if (request.action === 'recorderReady' && window.recorderStatus === 'recording') {
+    helpers.activeTabSendMessage({action: 'showRecordMessage', message: 'Recording Now'});
+  }
+});
